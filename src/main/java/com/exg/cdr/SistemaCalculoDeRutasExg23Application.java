298eeb0dff9cf28b044a71df4ca9146acf362409
Marcelo Molina <34018240+marmol-rm@users.noreply.github.com>
2023-07-06 11:11:37 -0600

    Creación del proyecto web

---
+++ src/main/java/com/exg/cdr/SistemaCalculoDeRutasExg23Application.java
@@ -0,0 +1,13 @@
+package com.exg.cdr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaCalculoDeRutasExg23Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaCalculoDeRutasExg23Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/exg/cdr/SistemaCalculoDeRutasExg23ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exg.cdr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaCalculoDeRutasExg23ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
